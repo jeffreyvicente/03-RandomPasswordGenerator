ecb6c204315cabdc02e23b4b411a9d43226c1e89
Jeffrey V <jeffreyvicente@hotmail.com>
2023-03-31 18:38:35 -0700

    Signed-off-by: Jeffrey V <jeffreyvicente@hotmail.com>

--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,11 @@ Create check for all the above
 var minPasswordLength = 8;
 var maxPasswordLength = 128;
 
+var characterLibrary = "";
+var alphaLowerLibrary = "abcdefghijklmnopqrstuvwxyz";
+var alphaUpperLibrary = "ABCDEFGHIJKLMNOPQRSTUVWXTZ";
+var symbolsLibrary = "!@#$%^&*()";
+
 //Prompts the user for the password length
 function passwordLength(){
   var length = prompt("How long do you want the password to be?");
@@ -19,6 +24,7 @@ function passwordLength(){
     passwordLength();
 
   }
+  return length;
 }
 
 //Prompts the user if the password requires lower case values
@@ -29,6 +35,7 @@ function lowerLetterCase(){
   }else{
     alert("You have chosen to EXCLUDE lower case characters!");
   }
+  return letterCaseInput;
 }
 
 //Prompts the user if the password requires upper case values
@@ -39,6 +46,7 @@ function upperLetterCase(){
   }else{
     alert("You have chosen to EXCLUDE upper case characters!");
   }
+  return letterCaseInput;
 }
 
 //Prompts the user if the password requires special values
@@ -49,9 +57,19 @@ function symbolsPrompt(){
   }else{
     alert("You have chosen to EXCLUDE special characters!");
   }
+  return letterCaseInput;
 }
 
+function passwordVerification(length, lower, upper, symbol){
 
+  console.log("This is a test of passing variables into a function " + length +" "+ lower + " " + upper + " " + symbol);
+  var userInput = confirm("Would you like to continue with the following password characters? \n\nPassord Length: " + length + "\nLower Case: " + lower + "\nUpper Case: " + upper + "\nSymbols: "+ symbol +"\n\nYes - Press OK \nNo - Press Cancel ");
+
+  if(userInput == false){
+    writePassword()
+  }
+
+}
 
 
 
@@ -60,11 +78,23 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  passwordLength();
-  lowerLetterCase();
-  upperLetterCase();
-  symbolsPrompt();
+  var length = passwordLength();
+  var lower = lowerLetterCase();
+  var upper = upperLetterCase();
+  var symbol = symbolsPrompt();
 
+ 
+  
+
+
+  //Console check to ensure return values from the methods above are returned properly.
+  console.log("This is the password lenght output "+ length);
+  console.log("This is the lower case output "+ lower);
+  console.log("This is the upper case output "+ upper);
+  console.log("This is the symbol output "+ symbol);
+
+
+  passwordVerification(length, lower, upper, symbol);
   //Orginal project code commented out for testing. 
   /* 
   var password = generatePassword();
